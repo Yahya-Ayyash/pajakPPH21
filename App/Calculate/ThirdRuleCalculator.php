50b1eb37a28ba323a203f13217373c5c69028f15
Yahya-Ayyash <110968484+Yahya-Ayyash@users.noreply.github.com>
2023-02-08 08:56:29 +0700

    terserah pesannya apa

---
+++ App/Calculate/ThirdRuleCalculator.php
@@ -0,0 +1,18 @@
+<?php
+//file: ThirdRuleCalculator.php
+
+class ThirdRuleCalculator extends AbstractCalculator
+{
+public function maxPkp(): float
+{
+return 500000000;
+}
+public function minPkp(): float
+{
+return 250000000;
+}
+public function taxPercentage(): float
+{
+return 0.25;
+}
+}
